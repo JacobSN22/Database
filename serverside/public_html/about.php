c2eac2da93051c3183bd34e5f3253843763670cb
Jacob <101559187+JacobSN22@users.noreply.github.com>
2022-11-24 14:31:49 +0100

    Delete serverside directory

--- serverside/public_html/about.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/assets/incl/init.php');
-
-$strPageTitle = 'Om os';
-require_once(DOCROOT . '/assets/incl/header.php');
-require_once(DOCROOT . '/assets/incl/footer.php');
-?>
--- serverside/public_html/contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/assets/incl/init.php');
-
-require_once(DOCROOT . '/assets/incl/header.php');
-
-require_once(DOCROOT . '/assets/incl/footer.php');
-?>
